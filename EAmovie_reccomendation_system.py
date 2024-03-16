829ce10ede107497ba7eef42038fcb94f3272ee4
Ernest Amponsah <kwaku@MacBook-Pro-89.cust.communityfibre.co.uk>
2024-03-16 23:40:56 +0000

    dictionary made from the txt file

--- EAmovie_reccomendation_system.py
+++ EAmovie_reccomendation_system.py
@@ -1,8 +1,38 @@
 import spacy
-import pandas as pd
 
 nlp = spacy.load('en_core_web_lg')
 
-df = pd.read_table('movies.txt')
+#opening the file and reading its contents
+file = open('movies.txt')
+movies = file.readlines()
+
+hulk = '''
+When the Hulk becomes too dangerous for the Earth,
+the Illuminati trick Hulk into a shuttle and launch him into space
+ to a planet where the Hulk can live in peace.
+Unfortunately, Hulk lands on the planet Sakaar 
+where he is sold into slavery and trained as a gladiator
+'''
+#creating a dictionary using from the txt file
+
+
+#initialising the empty dictionary
+movie_dict = {}
+
+#splitting the contents into lines
+movie_lines = movies #.strip()
+
+for line in movie_lines:
+
+    #splitting the line into key and value using :
+    parts = line.split(':')
+
+    key = parts[0].strip()
+    value = parts[1].strip()
+    #adding the movies to the dictionarys
+    movie_dict[key] = value
+
+#printing the dictionary to see if it worked
+#print(movie_dict['Movie D'])
 
 
