cefcfd5ac629d2e0e310be82c1fe166141c3aafe
Ernest Amponsah <kwaku@MacBook-Pro-89.cust.communityfibre.co.uk>
2024-03-17 02:27:36 +0000

    final product with the functions

--- EAmovie_reccomendation_system.py
+++ EAmovie_reccomendation_system.py
@@ -1,23 +1,14 @@
-import spacy
+import spacy #importing the language processing library
 
-nlp = spacy.load('en_core_web_lg')
+nlp = spacy.load('en_core_web_lg') #loading the large english language model
 
 #opening the file and reading its contents
 file = open('movies.txt')
 movies = file.readlines()
 
-hulk = '''
-When the Hulk becomes too dangerous for the Earth,
-the Illuminati trick Hulk into a shuttle and launch him into space
- to a planet where the Hulk can live in peace.
-Unfortunately, Hulk lands on the planet Sakaar 
-where he is sold into slavery and trained as a gladiator
-'''
 
 
 #creating a dictionary using from the txt file
-
-
 #initialising the empty dictionary
 movie_dict = {}
 
@@ -29,6 +20,7 @@ for line in movie_lines:
     #splitting the line into key and value using :
     parts = line.split(':')
 
+    #creating the key, value pairs
     key = parts[0].strip()
     value = parts[1].strip()
     #adding the movies to the dictionarys
@@ -39,28 +31,31 @@ for line in movie_lines:
 
 
 #The movie reccomendation system
-#def movie_reccomendation():
+def recommedation_system(movie_description):
 
-scores = {}
-#
-for movie, description in movie_dict.items():
-    #processing the descriptions
-    #tokenising each description
-    movie_doc = nlp(description)
-    #finding the similarity scores for all the movie descriptions
-    similarity = nlp(hulk).similarity(movie_doc)
+    movie_description = nlp(movie_description)
 
-    #whatever the similarity were extracting the key, value pair
-    scores[movie, description] = similarity
+    #dictionary to store the similarity scores
+    scores = {}
 
-#reccomending the film with the hoghest similarity
-reccomendation = max(scores, key=scores.get)
+    for movie, description in movie_dict.items():
+        #processing the descriptions
+        #tokenising each description
+        movie_doc = nlp(description)
+        #finding the similarity scores for all the movie descriptions
+        similarity = nlp(movie_description).similarity(movie_doc)
 
-print(f"Based on the description i would recommend this movie: {reccomendation}")
+        #whatever the similarity were extracting the key, value pair
+        scores[movie, description] = similarity
+
+    #reccomending the film with the hoghest similarity
+    recommendation = max(scores, key=scores.get)
+
+    print(f"Based on the description provided, I would recommend: {recommendation}")
 
 
 
 
 
-
+recomedation_system(hulk)
 
